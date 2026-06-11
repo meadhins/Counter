43f0bd8850ab3a1c0318c8b73c5250dfca55c6f4
meadhins <131248275+meadhins@users.noreply.github.com>
2026-06-11 16:46:29 +0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'counter-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch assets from cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
